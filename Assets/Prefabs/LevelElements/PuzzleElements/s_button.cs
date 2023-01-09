f3487fb758947fe7758e96faa427c99a3a3fcbbd
Isaac Dexter <isaacdexter@gmail.com>
2023-01-09 11:29:01 +0000

    Implemented the velocity based button, smashbutton, which triggers once so long as the object entering it is fast enough

--- Assets/Prefabs/LevelElements/PuzzleElements/s_button.cs
+++ Assets/Prefabs/LevelElements/PuzzleElements/s_button.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class s_button : s_trigger
 {
     /// <summary>How far to depress the button when its pressed</summary>
-    float m_depressionDistance = -0.1f;
+    protected float m_depressionDistance = -0.1f;
 
     /// <summary>A reference to the buttons plate</summary>
-    [SerializeField] private GameObject m_plate;
+    [SerializeField] protected GameObject m_plate;
 
     /// <summary>The material to use while the button is not pressed</summary>
-    [SerializeField] private Material m_materialUnpressed;
+    [SerializeField] protected Material m_materialUnpressed;
     /// <summary>The material to switch to while the button is pressed</summary>
-    [SerializeField] private Material m_materialPressed;
+    [SerializeField] protected Material m_materialPressed;
 
-    private void SetPlateMaterial(Material material)
+    protected void SetPlateMaterial(Material material)
     {
         m_plate.GetComponent<MeshRenderer>().material = material;
     }
@@ -32,37 +32,43 @@ public class s_button : s_trigger
         Release();
     }
 
-    private void Press()
+    protected void Press()
     {
         SetPlateMaterial(m_materialPressed);   //Switch to the pressed material
         m_plate.transform.position += new Vector3(0.0f, m_depressionDistance, 0.0f);   //Depress the button by depression distance
     }
 
-    private void Release()
+    protected void Release()
     {
         SetPlateMaterial(m_materialUnpressed);    //Switch to the unpressed material
         m_plate.transform.position += new Vector3(0.0f, -m_depressionDistance, 0.0f);   //press the button by negative depression distance
     }
-    
-    public void OnTriggerEnter(Collider other)
+
+    protected void OnTriggerEnter(Collider other)
+    {
+        if (CheckCollider(other))
+        {
+            Trigger();
+        }
+    }
+
+    virtual protected bool CheckCollider(Collider other)
     {
         if (!m_triggered) //Check the button isn't already pressed
         {
             if (other.tag == "Player")  //If the player is the one colliding with it, press it
             {
-                Trigger();
+                return true;
             }
         }
+        return false;
     }
 
-    public void OnTriggerExit(Collider other)
+    protected void OnTriggerExit(Collider other)
     {
-        if (m_triggered)
+        if (CheckCollider(other))
         {
-            if (other.tag == "Player")  //If the player is the one colliding with it, press it
-            {
-                Detrigger();
-            }
+            Detrigger();
         }
     }
 }
