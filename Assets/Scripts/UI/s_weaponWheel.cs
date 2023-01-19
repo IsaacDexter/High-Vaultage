884e3766779177011f911d13fa421b62aba8f5bf
Isaac Dexter <isaacdexter@gmail.com>
2023-01-19 13:29:38 +0000

    Updated weapon wheel script to be more encapsulated

--- Assets/Scripts/UI/s_weaponWheel.cs
+++ Assets/Scripts/UI/s_weaponWheel.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class s_weaponWheel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>If the weapon wheel is open at the moment.</summary>
+    private bool m_open = false;
+
+    /// <summary>The speed time should move at when the weapon wheel is open</summary>
+    private float m_timeDilation = 0.5f;
+
+    /// <summary>If the weapon wheel's open, close it and vice versa.</summary>
+    public void ToggleWeaponWheel()
     {
-        
+        if (m_open)
+        {
+            CloseWeaponWheel(); //If open, close
+        }
+        else
+        {
+            OpenWeaponWheel();  //If closed, open
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>Closes the weapon wheel and locks the cursor</summary>
+    private void CloseWeaponWheel()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;     //Lock and hide the cursor
+        m_open = false;  //Update check bool
+        Time.timeScale = 1f;        //Speed time up to the normal amount
+    }
+
+    /// <summary>Opens the weapons wheel, freeing the cursor and slowing down time</summary>
+    private void OpenWeaponWheel()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;      //Unlock and show the cursor
+        m_open = true;   //Update check bool
+        Time.timeScale = m_timeDilation;      //Slow down time to the slow speed
     }
 }
--- Assets/Scripts/Weapons/s_weaponManager.cs
+++ Assets/Scripts/Weapons/s_weaponManager.cs
@@ -8,18 +8,21 @@ public class s_weaponManager : MonoBehaviour
     [SerializeField] s_hand m_leftHand;
     /// <summary>The right hand, which holds the right weapon</summary>
     [SerializeField] s_hand m_rightHand;
+    /// <summary>A reference to a s_weaponwheel class, which handles opening events etc</summary>
+    private s_weaponWheel m_weaponWheel = new s_weaponWheel();
 
-    /// <summary>If the weapon wheel is open at the moment.</summary>
-    private bool m_weaponWheelOpen = false;
+    /// <summary>The key to press to fire the left weapon</summary>
+    private KeyCode m_primaryFireKey = KeyCode.Mouse0;
     /// <summary>The key to press to open the weapon wheel</summary>
     private KeyCode m_weaponWheelOpenKey = KeyCode.Mouse2;
-    /// <summary>The speed time should move at when the weapon wheel is open</summary>
-    private float m_weaponWheelTimeDilation = 0.5f;
+
+
+
 
     /// <summary>Handles the users input events and prompts the hands to regenerate charge</summary>
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(m_weaponWheelOpenKey))
         {
             m_leftHand.PullTrigger();
         }
@@ -38,7 +41,7 @@ public class s_weaponManager : MonoBehaviour
 
         if (Input.GetKeyDown(m_weaponWheelOpenKey))
         {
-            ToggleWeaponWheel();
+            m_weaponWheel.ToggleWeaponWheel();
         }
     }
     public void SwitchWeapon(GameObject weapon, float arm)
@@ -51,36 +54,5 @@ public class s_weaponManager : MonoBehaviour
         {
             m_rightHand.Equip(weapon);
         }
-    }
-
-    /// <summary>If the weapon wheel's open, close it and vice versa.</summary>
-    public void ToggleWeaponWheel()
-    {
-        if (m_weaponWheelOpen)  
-        {
-            CloseWeaponWheel(); //If open, close
-        }
-        else
-        {
-            OpenWeaponWheel();  //If closed, open
-        }
-    }
-
-    /// <summary>Closes the weapon wheel and locks the cursor</summary>
-    private void CloseWeaponWheel()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;     //Lock and hide the cursor
-        m_weaponWheelOpen = false;  //Update check bool
-        Time.timeScale = 1f;        //Speed time up to the normal amount
-    }
-
-    /// <summary>Opens the weapons wheel, freeing the cursor and slowing down time</summary>
-    private void OpenWeaponWheel()
-    {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;      //Unlock and show the cursor
-        m_weaponWheelOpen = true;   //Update check bool
-        Time.timeScale = m_weaponWheelTimeDilation;      //Slow down time to the slow speed
     }
 }
