b0fe377989d5bf8a80aca41e50961815aa2a5e57
Isaac Dexter <isaacdexter@gmail.com>
2023-02-01 18:54:46 +0000

    Implemented a partially functional settings menu and overhauled the design. The majority of things are non functionaly at the moment as they affect the wrong pipeline! pass in a reference to the universal rendering pipeline to be able to affect its values. Anything else works fine though, and fixing should be no problem

--- Assets/Scripts/PostProcessing/s_brightness.cs
+++ Assets/Scripts/PostProcessing/s_brightness.cs
@@ -13,6 +13,10 @@ public class s_brightness : MonoBehaviour
     private VolumeProfile m_profile;
     [Tooltip("The profiles lift gamma gain overrides, which can be used to adjust 'brightness' of the scene.")]
     private LiftGammaGain m_liftGammaGain;
+
+    private Bloom m_bloom;
+
+    private MotionBlur m_motionBlur;
     private void Start()
     {
         //Get the volume tied to this component, if not set manually.
@@ -27,6 +31,18 @@ public class s_brightness : MonoBehaviour
             lgg = m_profile.Add<LiftGammaGain>(false);
         }
         m_liftGammaGain = lgg;
+
+        if (!m_profile.TryGet<MotionBlur>(out var mb))  //Attempt to get the motion blur. This will make a new one if no override has been set.
+        {
+            mb = m_profile.Add<MotionBlur>(false);
+        }
+        m_motionBlur = mb;
+
+        if (!m_profile.TryGet<Bloom>(out var b))  //Attempt to get the bloom. This will make a new one if no override has been set.
+        {
+            b = m_profile.Add<Bloom>(false);
+        }
+        m_bloom = b;
     }
 
     /// <summary>Set's the shared profile's gain's alpha component, which will increase the brightness of the scene. </summary>
@@ -34,5 +50,33 @@ public class s_brightness : MonoBehaviour
     public void SetGain(float gain)
     {
         m_liftGammaGain.gain.Override(new Vector4(1f, 1f, 1f, gain));
+    }
+
+    public void SetBloom(bool bloom)
+    {
+        float intensity;
+        if (bloom)
+        {
+            intensity = 1.0f;
+        }
+        else
+        {
+            intensity = 0.0f;
+        }
+        m_bloom.intensity.Override(intensity);
+    }
+
+    public void SetMotionBlur(bool motionBlur)
+    {
+        float intensity;
+        if (motionBlur)
+        {
+            intensity = 0.5f;
+        }
+        else
+        {
+            intensity = 0.0f;
+        }
+        m_motionBlur.intensity.Override(intensity);
     }
 }
--- Assets/Scripts/UI/s_settings.cs
+++ Assets/Scripts/UI/s_settings.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using UnityEngine.Rendering;
 using TMPro;
 
 public class s_settings : s_menu
@@ -9,8 +10,20 @@ public class s_settings : s_menu
     int m_resolutionX, m_resolutionY;
     FullScreenMode m_fullscreenMode;
     float m_brightness;
+    int m_vsync;
+    int m_antiAliasing;
+    int? m_qualityLevel;
+    int m_textureRes;
+    ShadowQuality m_shadowQuality;
+    ShadowResolution m_shadowResolution;
+    int m_shadowCascades;
+    float m_shadowDistance;
+    bool m_bloom;
+    bool m_motionBlur;
+
     public s_brightness m_postProcess;
     public s_menu m_pause;
+    [SerializeField] private GameObject m_advancedSettings;
     protected override void Start()
     {
         base.Start();
@@ -22,6 +35,16 @@ public class s_settings : s_menu
         m_resolutionY = Screen.height;
         m_fullscreenMode = FullScreenMode.FullScreenWindow;
         m_brightness = 0.0f;
+        m_qualityLevel = QualitySettings.GetQualityLevel();
+        m_vsync = QualitySettings.vSyncCount;
+        m_antiAliasing = QualitySettings.antiAliasing;
+        m_textureRes = QualitySettings.masterTextureLimit;
+        m_shadowQuality = QualitySettings.shadows;
+        m_shadowResolution = QualitySettings.shadowResolution;
+        m_shadowCascades = QualitySettings.shadowCascades;
+        m_shadowDistance = QualitySettings.shadowDistance;
+        m_bloom = false;
+        m_motionBlur = false;
 
         //Apply();
     }
@@ -41,7 +64,26 @@ public class s_settings : s_menu
     {
         Screen.SetResolution(m_resolutionX, m_resolutionY, m_fullscreenMode);
         m_postProcess.SetGain(m_brightness);
+        m_postProcess.SetBloom(m_bloom);
+        m_postProcess.SetMotionBlur(m_motionBlur);
+        if (m_qualityLevel != null)
+        {
+            QualitySettings.SetQualityLevel(m_qualityLevel.Value);
+        }
+        else
+        {
+            QualitySettings.vSyncCount = m_vsync;
+            QualitySettings.antiAliasing = m_antiAliasing;
+            QualitySettings.masterTextureLimit = m_textureRes;
+            QualitySettings.shadows = m_shadowQuality;
+            QualitySettings.shadowResolution = m_shadowResolution;
+            QualitySettings.shadowCascades = m_shadowCascades;
+            QualitySettings.shadowDistance = m_shadowDistance;
+            
+        }
     }
+
+    #region WindowSettings
 
     public void SetScreenResolution(TextMeshProUGUI dropdown)
     {
@@ -50,28 +92,147 @@ public class s_settings : s_menu
         m_resolutionY = int.Parse(resolution.Substring(resolution.IndexOf('×') + 1, resolution.Length - (resolution.IndexOf('×') + 1)));
     }
 
-    public void SetFullscreen(TextMeshProUGUI dropdown)
+    public void SetVsync(bool value)
     {
-        string fullscreen = dropdown.text;
-        switch (fullscreen)
+        if (value)
         {
-            case "Fullscreen":
+            m_vsync = 1;
+        }
+        else 
+        {
+            m_vsync = 0;
+        }
+    }
+
+
+    public void SetFullscreen(int value)
+    {
+        switch (value)
+        {
+            case 0:
                 m_fullscreenMode = FullScreenMode.ExclusiveFullScreen;
                 break;
-            case "Windowed":
+            case 1:
                 m_fullscreenMode = FullScreenMode.Windowed;
                 break;
-            case "Windowed Borderless":
+            case 2:
                 m_fullscreenMode = FullScreenMode.FullScreenWindow;
                 break;
             default:
                 break;
         }
-        print(m_fullscreenMode);
+    }
+
+    #endregion
+
+    #region BasicSettings
+
+    public void SetAntiAliasing(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                m_antiAliasing = 0;
+                break;
+            default:
+                m_antiAliasing = 2 ^ value;
+                break;
+        }
+    }
+
+    public void SetQualityLevel(int value)
+    {
+        if (value < 3)
+        {
+            m_qualityLevel = value;
+            m_advancedSettings.SetActive(false);
+        }
+        else
+        {
+            m_qualityLevel = null;
+            m_advancedSettings.SetActive(true);
+        }
     }
 
     public void SetBrightness(float value)
     {
         m_brightness = value;
     }
+
+    #endregion
+
+    #region AdvancedSettings
+
+    public void SetRealTimeShadows(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                m_shadowQuality = ShadowQuality.Disable;
+                break;
+            case 1:
+                m_shadowQuality = ShadowQuality.HardOnly;
+                break;
+            case 2:
+                m_shadowQuality = ShadowQuality.All;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    public void SetShadowResolution(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                m_shadowResolution = ShadowResolution.Low;
+                break;
+            case 1:
+                m_shadowResolution = ShadowResolution.Medium;
+                break;
+            case 2:
+                m_shadowResolution = ShadowResolution.High;
+                break;
+            case 3:
+                m_shadowResolution = ShadowResolution.VeryHigh;
+                break;
+            default:
+                break;
+        }
+    }
+    public void SetShadowCascades(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                m_shadowCascades = 0;
+                break;
+            default:
+                m_shadowCascades = 2 ^ value;
+                break;
+        }
+    }
+
+    public void SetShadowDistance(float value)
+    {
+        m_shadowDistance = value;
+    }
+
+    public void SetBloom(bool value)
+    {
+        m_bloom = value;
+    }
+    
+    public void SetMotionBlur(bool value)
+    {
+        m_motionBlur = value;
+    }
+    public void SetTextureQuality(int value)
+    {
+        m_textureRes = value;
+    }
+
+    #endregion
+
 }
