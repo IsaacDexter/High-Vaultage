d532fa321effab0e114f3047f4a7e1a3f3205e3a
Logan Bryan <92024998+LoganBryan@users.noreply.github.com>
2023-02-09 09:09:57 +0000

    Updated player animator controller

--- Assets/Scripts/Player/s_PlayerAnimator.cs
+++ Assets/Scripts/Player/s_PlayerAnimator.cs
@@ -4,48 +4,76 @@ using UnityEngine;
 
 public class s_PlayerAnimator : MonoBehaviour
 {
+    // Animators
     private Animator m_leftAnimator;
     private Animator m_rightAnimator;
+
+    // The player script
+    private s_player player;
+
+    // Hand scripts
+    private s_hand m_leftHand;
+    private s_hand m_rightHand;
 
     [Header("Arms")]
     [SerializeField] GameObject m_leftArm;
     [SerializeField] GameObject m_rightArm;
+    [SerializeField] GameObject m_leftHandEmpty;
+    [SerializeField] GameObject m_rightHandEmpty;
 
     void Start()
     {
         m_leftAnimator = m_leftArm.GetComponent<Animator>();
         m_rightAnimator = m_rightArm.GetComponent<Animator>();
 
+        player = GetComponent<s_player>();
+        m_leftHand = m_leftHandEmpty.GetComponent<s_hand>();
+        m_rightHand = m_rightHandEmpty.GetComponent<s_hand>();
     }
 
     void Update()
     {
         HandleAnimations();
+        HandleWeaponAnimations();
     }
 
     private void HandleAnimations()
     {
-        if (gameObject.GetComponent<s_player>().m_moveDirection == Vector3.zero)
+        if (player.m_moveDirection == Vector3.zero)
         {
             m_leftAnimator.SetFloat("MovementSpeed", 0);
             m_rightAnimator.SetFloat("MovementSpeed", 0);
         }
-        else if (gameObject.GetComponent<s_player>().m_moveDirection != Vector3.zero && gameObject.GetComponent<s_player>().m_grounded && !gameObject.GetComponent<s_player>().m_sliding)
+        else if (player.m_moveDirection != Vector3.zero && player.m_grounded && !player.m_sliding)
         {
             m_leftAnimator.SetFloat("MovementSpeed", 1);
             m_rightAnimator.SetFloat("MovementSpeed", 1);
         }
 
-        if (gameObject.GetComponent<s_player>().m_sliding)
+        if (player.m_sliding)
         {
             m_leftAnimator.SetTrigger("StartSlide");
             m_rightAnimator.SetTrigger("StartSlide");
 
         }
-        else if (!gameObject.GetComponent<s_player>().m_sliding)
+        else if (!player.m_sliding)
         {
             m_leftAnimator.ResetTrigger("StartSlide");
             m_rightAnimator.ResetTrigger("StartSlide");
+        }
+    }
+
+    private void HandleWeaponAnimations()
+    {
+        switch (m_leftHand.m_weapon.name)
+        {
+            case "aaa":
+
+                break;
+
+            default:
+                Debug.Log(m_leftHand.m_weapon.name);
+                break;
         }
     }
 }
