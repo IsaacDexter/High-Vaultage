4bf9daa2fc2e2ec15075eb11c72927580683673e
Logan Bryan <92024998+LoganBryan@users.noreply.github.com>
2023-02-09 11:56:00 +0000

    Updated main menu

--- Assets/Menu/Scripts/MainMenu.cs
+++ Assets/Menu/Scripts/MainMenu.cs
@@ -17,6 +17,8 @@ public class MainMenu : s_menu
     public GameObject LerpScript;
     [SerializeField] private s_settings m_settings;
 
+    [SerializeField] public List<string> levelLists;
+
     // Start is called before the first frame update
     override protected void Start()
     {
@@ -40,10 +42,10 @@ public class MainMenu : s_menu
     {
         UI.enabled = false;
 
-        SceneManager.LoadScene("levelCor1");
-        SceneManager.LoadScene("PlayerControllerScene");
+        //SceneManager.LoadScene("levelCor1");
+        //SceneManager.LoadScene("PlayerControllerScene");
         
-        //StartCoroutine(LerpScript.GetComponent<LerpScript>().LevelSelectLerp(UILevel));
+        StartCoroutine(LerpScript.GetComponent<LerpScript>().LevelSelectLerp(UILevel));
     }
 
     public void OpenOptions()
@@ -66,8 +68,8 @@ public class MainMenu : s_menu
         UI2.enabled = true;
         UILevel.enabled = false;
         StartCoroutine(LerpScript.GetComponent<LerpScript>().LevelSelectedLerp(Player));
+
+
+
     }
-
-
-
 }
